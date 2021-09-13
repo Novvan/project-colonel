59adcd3ef909220c0fe84e1c858b3dbe3a4f6fba
Fabrizio <fabriziobarbieri@hotmail.com>
2021-09-13 16:30:18 -0300

    basic scripts

---
+++ Assets/Scripts/PlayerScripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    public void Move(Vector3 dir) 
+    {
+        dir.y = 0;
+        dir *= speed;
+        dir.y = _rb.velocity.y;
+        _rb.velocity = dir;
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class CharacterController : MonoBehaviourPun
+{
+    private Character _character;
+
+    private void Awake()
+    {
+        _character = GetComponent<Character>();
+        if (!photonView.IsMine) Destroy(this);
+    }
+    private void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, 0, v);
+        if (v != 0 || h != 0) _character.Move(dir);
+    }
+}
