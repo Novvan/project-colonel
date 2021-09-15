8257671877274d27d6be5dc4d690a5e2d587fb2c
Fabrizio <fabriziobarbieri@hotmail.com>
2021-09-14 21:27:41 -0300

    Zombie Controller ultra basic

---
+++ Assets/Scripts/PlayerScripts/AIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class AIController : MonoBehaviourPun
+{
+    [SerializeField] private GameObject target;
+    private Rigidbody _rb;
+    private Character _character;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _character = GetComponent<Character>();
+    }
+    void Update()
+    {
+        if (target != null) 
+        {
+            Vector3 targerDir = target.transform.position - transform.position;
+            _character.Move(targerDir.normalized);
+        }
+    }
+}
