5f2108adb5a922450331131e3092f90b0852bc2a
Ian Geier <ianallengeier@gmail.com>
2021-09-24 19:55:11 -0300

    destroy

--- Assets/Scripts/Spawners/Spawner.cs
+++ Assets/Scripts/Spawners/Spawner.cs
@@ -7,6 +7,12 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private string _prefabName;
     [SerializeField] protected GameManager gm;
+
+    private void Start()
+    {
+        if (!PhotonNetwork.IsMasterClient) Destroy(gameObject);
+    }
+
     public virtual void Spawn()
     {
         GameObject temp = PhotonNetwork.Instantiate(_prefabName, gameObject.transform.position, Quaternion.identity);
